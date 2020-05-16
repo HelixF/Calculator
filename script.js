dc0f3763883f65850247dc902ee7b43bcf92af42
Florian Soller <flsoller@gmail.com>
2020-05-16 23:37:36 +0200

    Create basic math operators.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+function add(num1, num2) {
+    let result = num1 + num2;
+    return result;
+}
+
+function subtract(num1, num2) {
+    let result = num1 - num2;
+    return result;
+}
+
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
+
+function divide(num1, num2) {
+    let result = num1 / num2;
+    return result;
+}
+
+console.log(add(6, 8));
