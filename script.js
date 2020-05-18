2d425b97487225112d0201274f0e2ad5e719866c
Florian Soller <flsoller@gmail.com>
2020-05-18 18:29:09 +0200

    Add operate function.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Define math functions.
 function add(num1, num2) {
     let result = num1 + num2;
     return result;
@@ -18,4 +19,25 @@ function divide(num1, num2) {
     return result;
 }
 
-console.log(add(6, 8));
+
+// Calling the right math function based on operator.
+function operate(operator, num1, num2) {
+     if (operator === '+') {
+         console.log(add(num1, num2));
+     }
+     else if (operator === '-') {
+         console.log(subtract(num1, num2));
+     }
+     else if (operator === '*') {
+         console.log(multiply(num1, num2));
+     }
+     else if (operator === '/') {
+         console.log(divide(num1, num2));
+     }
+     else {
+        console.log('Math function not supported or recognized.');
+     }
+}
+
+// Test...
+operate('/', 25, 2);
