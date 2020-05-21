59bfc056184707025cdfeba7f8a98912ff8dde47
Florian Soller <flsoller@gmail.com>
2020-05-21 23:17:50 +0200

    Implement calculator functionality w/ placeholder. Changed background color.

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-// Asaign caluclator display variable.
+// Initialize DOM elements
 const displayValue = document.getElementById('calcDisplay');
+const numberBtn = document.getElementsByClassName('numberBtn');
+const clearBtn = document.getElementById('btnAC');
+const plusBtn = document.getElementById('btnPlus');
+const minusBtn = document.getElementById('btnMinus');
+const multiplyBtn = document.getElementById('btnMultiply');
+const divideBtn = document.getElementById('btnDivide');
+const equalBtn = document.getElementById('btnEquals');
+
+// Default display value on startup
+displayValue.innerHTML = 0;
+
+// Global variables
+let operandOne = 0;
+let operandTwo = 0;
+let operatorSelected = 0;
+
 
 
 // Define math functions.
@@ -27,16 +43,16 @@ function divide(num1, num2) {
 // Calling math function based on operator.
 function operate(operator, num1, num2) {
      if (operator === '+') {
-         console.log(add(num1, num2));
+         return(add(num1, num2));
      }
      else if (operator === '-') {
-         console.log(subtract(num1, num2));
+         return(subtract(num1, num2));
      }
      else if (operator === '*') {
-         console.log(multiply(num1, num2));
+         return(multiply(num1, num2));
      }
      else if (operator === '/') {
-         console.log(divide(num1, num2));
+         return(divide(num1, num2));
      }
      else {
         console.log('Math function not supported or recognized.');
@@ -44,24 +60,59 @@ function operate(operator, num1, num2) {
 }
 
 // Add event listeners to number buttons.
-let numberBtn = document.getElementsByClassName('numberBtn');
-
 for (let i = 0 ; i < numberBtn.length ; i++) {
     numberBtn[i].addEventListener('click', () => {
         numberPressed(i);
     });
 }
 
-// Append numberBtn value to display.
-// function numberPressed(num) {
-//     if ((displayValue.innerText = '0') || (displayValue.innerText = 0)) {
-//         displayValue.innerText = num;
-//     }
-//     displayValue.innerText = displayValue.innerText + num;
-   
-//     console.log(displayValue.innerText);
-// }
+//Append numberBtn value to display.
+function numberPressed(num) {
 
+    // If OperandOne has been set before, clear display and let user enter second number.
+    if (operandOne === displayValue.innerHTML) {
+        displayValue.innerHTML = 0;
+        displayValue.innerHTML += num;
+        let output = parseInt(displayValue.innerHTML, 10);
+        displayValue.innerHTML = output;
+        return;
+    }
+    // Add the pressed number to display value.
+    displayValue.innerHTML += num;
+    // Remove leading zero and re-assign.
+    let output = parseInt(displayValue.innerHTML, 10);
+    displayValue.innerHTML = output;
+}
+
+// Clear all button.
+clearBtn.addEventListener('click', ClearAll);
+
+function ClearAll() {
+    displayValue.innerHTML = 0;
+    operandOne = 0;
+    operandTwo = 0;
+    operatorSelected = 0;
+}
+
+
+// Add event listeners to operator buttons
+plusBtn.addEventListener('click' , operatorPressed);
+
+function operatorPressed(){
+    operandOne = displayValue.innerHTML;
+    operatorSelected = plusBtn.innerHTML;
+}
+
+// Equal sign pressed.
+equalBtn.addEventListener('click', evaluateMath);
+
+function evaluateMath() {
+    operandTwo = displayValue.innerHTML;
+
+    let result = operate(operatorSelected, parseInt(operandOne), parseInt(operandTwo));
+    displayValue.innerHTML = result;
+    operandOne = result;
+}
 
 
 
